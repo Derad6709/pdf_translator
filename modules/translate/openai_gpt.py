5f50c7b8edbd55d518d79968132eca2159978dfb
Derad6709 <Derad6709@users.noreply.github.com>
2024-05-12 06:22:00 +0300

    Update openai_gpt.py

--- modules/translate/openai_gpt.py
+++ modules/translate/openai_gpt.py
@@ -1,99 +1,16 @@
 from .base import TranslateBase 
 from openai import OpenAI
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100-12B-avg-5-ckpt")
+tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100-12B-avg-5-ckpt")
+
 
 def system_prompt(from_lang, to_lang):
     p  = "You are an %s-to-%s translator. " % (from_lang, to_lang)
     p += "Keep all special characters and HTML tags as in the source text. Return only %s translation." % to_lang
     return p
 
-langs = [
-    "Albanian",
-    "Arabic",
-    "Armenian",
-    "Awadhi",
-    "Azerbaijani",
-    "Bashkir",
-    "Basque",
-    "Belarusian",
-    "Bengali",
-    "Bhojpuri",
-    "Bosnian",
-    "Brazilian Portuguese",
-    "Bulgarian",
-    "Cantonese (Yue)",
-    "Catalan",
-    "Chhattisgarhi",
-    "Chinese",
-    "Croatian",
-    "Czech",
-    "Danish",
-    "Dogri",
-    "Dutch",
-    "English",
-    "Estonian",
-    "Faroese",
-    "Finnish",
-    "French",
-    "Galician",
-    "Georgian",
-    "German",
-    "Greek",
-    "Gujarati",
-    "Haryanvi",
-    "Hindi",
-    "Hungarian",
-    "Indonesian",
-    "Irish",
-    "Italian",
-    "Japanese",
-    "Javanese",
-    "Kannada",
-    "Kashmiri",
-    "Kazakh",
-    "Konkani",
-    "Korean",
-    "Kyrgyz",
-    "Latvian",
-    "Lithuanian",
-    "Macedonian",
-    "Maithili",
-    "Malay",
-    "Maltese",
-    "Mandarin",
-    "Mandarin Chinese",
-    "Marathi",
-    "Marwari",
-    "Min Nan",
-    "Moldovan",
-    "Mongolian",
-    "Montenegrin",
-    "Nepali",
-    "Norwegian",
-    "Oriya",
-    "Pashto",
-    "Persian (Farsi)",
-    "Polish",
-    "Portuguese",
-    "Punjabi",
-    "Rajasthani",
-    "Romanian",
-    "Russian",
-    "Sanskrit",
-    "Santali",
-    "Serbian",
-    "Sindhi",
-    "Sinhala",
-    "Slovak",
-    "Slovene",
-    "Slovenian",
-    "Ukrainian",
-    "Urdu",
-    "Uzbek",
-    "Vietnamese",
-    "Welsh",
-    "Wu"
-]
-
+langs = ['af', 'am', 'ar', 'ast', 'az', 'ba', 'be', 'bg', 'bn', 'br', 'bs', 'ca', 'ceb', 'cs', 'cy', 'da', 'de', 'el', 'en', 'es', 'et', 'fa', 'ff', 'fi', 'fr', 'fy', 'ga', 'gd', 'gl', 'gu', 'ha', 'he', 'hi', 'hr', 'ht', 'hu', 'hy', 'id', 'ig', 'ilo', 'is', 'it', 'ja', 'jv', 'ka', 'kk', 'km', 'kn', 'ko', 'lb', 'lg', 'ln', 'lo', 'lt', 'lv', 'mg', 'mk', 'ml', 'mn', 'mr', 'ms', 'my', 'ne', 'nl', 'no', 'ns', 'oc', 'or', 'pa', 'pl', 'ps', 'pt', 'ro', 'ru', 'sd', 'si', 'sk', 'sl', 'so', 'sq', 'sr', 'ss', 'su', 'sv', 'sw', 'ta', 'th', 'tl', 'tn', 'tr', 'uk', 'ur', 'uz', 'vi', 'wo', 'xh', 'yi', 'yo', 'zh', 'zu']
 class TranslateOpenAIGPT(TranslateBase):
     def init(self, cfg: dict):
         self.client = OpenAI(api_key=cfg['openai_api_key'])
@@ -101,15 +18,10 @@ class TranslateOpenAIGPT(TranslateBase):
     def get_languages(self):
         return langs
 
-    def translate(self, text: str, from_lang='ENGLISH', to_lang='SLOVENIAN') -> str:
-        response = self.client.chat.completions.create(
-            model='gpt-4-1106-preview',
-            temperature=0.2,
-            messages=[
-                { 'role': 'system', 'content': system_prompt(from_lang, to_lang) },
-                { 'role': 'user', 'content': text },
-            ]
-        )
+    def translate(self, text: str, from_lang='en', to_lang='si') -> str:
+        tokenizer.src_lang = text
+        encoded_hi = tokenizer(hi_text, return_tensors="pt")
+        generated_tokens = model.generate(**encoded_hi, forced_bos_token_id=tokenizer.get_lang_id(to_lang))
+        translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
 
-        translated_text = response.choices[0].message.content
         return translated_text
